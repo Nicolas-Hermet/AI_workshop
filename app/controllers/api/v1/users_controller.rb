5683de23e541165c4dfb7f044546682138582201
Yoni Levy <9082902+yocoman31@users.noreply.github.com>
2026-06-12 11:06:35 +0200

    Update users_controller.rb
    Faille Sensitive Data Exposure

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,16 @@
 module Api
   module V1
     class UsersController < BaseController
+      # On définit les champs publics que l'on accepte de renvoyer
+      USER_PUBLIC_FIELDS = %i[id name email role bio].freeze
+
       def show
         @user = User.find(params[:id])
-        render json: @user
+        render json: @user.as_json(only: USER_PUBLIC_FIELDS)
       end
 
       def index
-        render json: User.all
+        render json: User.all.as_json(only: USER_PUBLIC_FIELDS)
       end
     end
   end
